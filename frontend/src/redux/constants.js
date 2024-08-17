522c26b457962048d697de8249b50936389f2f97
Sushant <sushantsinghchauhan08@gmail.com>
2024-08-18 01:42:50 +0530

    frontend updated

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,2 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const BASE_URL = process.env.VITE_API_URL || "";
+export const BASE_URL = import.meta.env.VITE_API_URL || "";
 export const USERS_URL = "/api/users";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": {
-        target: process.env.VITE_API_URL || "http://localhost:3030",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
   },
 });
