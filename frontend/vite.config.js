018ec89f2b2a5aa636da0154e8a1689d8ebea10f
Sushant <sushantsinghchauhan08@gmail.com>
2024-08-18 02:03:48 +0530

    frontend updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+  server: {
+    proxy: {
+      "/api/": {
+        target: "https://mern-8991.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
